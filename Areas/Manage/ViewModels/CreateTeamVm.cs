e445cc2d70bb9f6109ece976ae22e357dc98f3fd
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2024-01-21 13:18:29 +0400

    some changes

--- Areas/Manage/ViewModels/CreateTeamVm.cs
+++ Areas/Manage/ViewModels/CreateTeamVm.cs
@@ -1,10 +1,24 @@
-﻿namespace Lumia.Areas.Manage.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lumia.Areas.Manage.ViewModels
 {
     public class CreateTeamVm
     {
+        [Required]
+        [MinLength(7)]
+        [MaxLength(50)]
         public string FullName { get; set; }
+
+        [Required]
+        [MinLength(7)]
+        [MaxLength(50)]
         public string Word { get; set; }
+
+        [Required]
+        [MinLength(7)]
+        [MaxLength(50)]
         public string Description { get; set; }
+        [Required]
         public IFormFile? File { get; set; }
     }
 }
--- Areas/Manage/ViewModels/UpdateTeamVm.cs
+++ Areas/Manage/ViewModels/UpdateTeamVm.cs
@@ -1,11 +1,29 @@
-﻿namespace Lumia.Areas.Manage.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lumia.Areas.Manage.ViewModels
 {
     public class UpdateTeamVm
     {
+
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(7)]
+        [MaxLength(50)]
         public string FullName { get; set; }
+
+        [Required]
+        [MinLength(7)]
+        [MaxLength(50)]
         public string Word { get; set; }
+
+        [Required]
+        [MinLength(7)]
+        [MaxLength(50)]
         public string Description { get; set; }
+
+
+        [Required]
         public IFormFile? File { get; set; }
     }
 }
